3460f96f9b9b8a4abd76763106a257acf08f345c
Arnaud <arnaud.dumanois@etu.univ-cotedazur.fr>
2024-01-29 16:38:49 +0100

    saut fonctionnel par contre j'ai trouvé des bug avec le fog of war

--- front/js/game_local_1v1/referee.js
+++ front/js/game_local_1v1/referee.js
@@ -8,6 +8,7 @@ function beginningPositionIsValid(currentPlayer, position) {
 }
 
 function moveIsValid(oldPosition, cell) {
+    console.log(getAdjacentCells(oldPosition));
     if(cell.classList.contains("occupied")){
         alert("Cette cellule est déjà occupée. Choisissez une autre.");
         return false;
@@ -19,6 +20,16 @@ function moveIsValid(oldPosition, cell) {
 
     let oldPositionLine = parseInt(oldPosition[0]);
     let oldPositionColumn = parseInt(oldPosition[2]);
+
+    const adjacentPlayer = findAdjacentPlayer(newPosition);
+    if(adjacentPlayer){
+        const wallBehindAdjacentPlayer = findWallBehindPlayer(adjacentPlayer);
+        if (wallBehindAdjacentPlayer) {
+            alert("Vous ne pouvez pas sauter il y a un mur derrière le joueur");
+            return false;
+        }
+        return true;
+    }
 
     if (!(-1 < newPositionLine < 9 && -1 < newPositionColumn < 9)){
         alert("chuckles wtf");
@@ -66,4 +77,37 @@ function moveIsValid(oldPosition, cell) {
     }
     alert("Votre position actuel ne permet pas ce mouvement");
     return false;
+}
+
+function getAdjacentCells(position) {
+    let line = parseInt(position[0]);
+    let column = parseInt(position[2]);
+
+    // ajouter les cases adjacentes
+    const adjacentCells = [];
+    if (line > 0) {
+        adjacentCells.push(document.getElementById(`${line - 1}-${column}~cell`));
+    }
+    if (line < 8) {
+        adjacentCells.push(document.getElementById(`${line + 1}-${column}~cell`));
+    }
+    if (column > 0) {
+        adjacentCells.push(document.getElementById(`${line}-${column - 1}~cell`));
+    }
+    if (column < 8) {
+        adjacentCells.push(document.getElementById(`${line}-${column + 1}~cell`));
+    }
+    return adjacentCells;
+}
+function findAdjacentPlayer(position){
+    const adjacentCells = getAdjacentCells(position);
+    for (const cell of adjacentCells) {
+        if(cell.classList.contains("occupied"))
+            return cell;
+    }
+    return false;
+}
+
+function findWallBehindPlayer(adjacentPlayer) {
+    console.log("adjacentPlayer", adjacentPlayer);
 }
