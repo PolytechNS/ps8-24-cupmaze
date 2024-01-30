b44362a70a77b8af25944eafd38a2847f93b5727
simonbeurel <simon.beurel@etu.unice.fr>
2024-01-30 15:24:15 +0000

    Ajout de quelque documentation pour le fichier socketGameBot.js

--- back/Sockets/socketGameBot.js
+++ back/Sockets/socketGameBot.js
@@ -1,11 +1,18 @@
 const AIEasy = require("../logic/ai.js");
 const { Server } = require("socket.io");
 
+/**
+ * Cette fonction va servir pour pouvoir créer le socket qui correspond à quand on va vouloir initialiser une partie entre le bot et un joueur en local
+ * Cette fonction est appelée dans le fichier "index.js" pour l'initialiser en attendant une connexion d'un client
+ * @param server
+ */
 function createSocket(server){
+    //On passe en paramètre le serveur que l'on reçoit en paramètre de la fonction
     const io = new Server(server);
     io.on("connection", (socket) => {
         console.log("a user connected");
 
+        //Quand on reçoit un message "newMove" alors on doit demander à l'IA de jouer et on envoie le résultat au client (io.emit)
         socket.on("newMove", (msg) => {
             console.log("Player1 make a new movement, we will compute the AI's move and send it back to the client");
             let newPosition = AIEasy.computeMove(msg);
