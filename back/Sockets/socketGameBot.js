86744a1928dc8edd4e6ef0baf142d50f04cb5d0e
simonbeurel <simon.beurel@etu.univ-cotedazur.fr>
2024-02-05 10:42:32 +0100

    Préparation des différents appels sockets qui seront réalisés

--- back/Sockets/socketGameBot.js
+++ back/Sockets/socketGameBot.js
@@ -1,6 +1,6 @@
 const AIEasy = require("../logic/ai.js");
 const { Server } = require("socket.io");
-const Game = require("../logic/Game.js");
+const { Game } = require("../logic/Game.js");
 
 /**
  * Cette fonction va servir pour pouvoir créer le socket qui correspond à quand on va vouloir initialiser une partie entre le bot et un joueur en local
@@ -8,14 +8,37 @@ const Game = require("../logic/Game.js");
  * @param server
  */
 
+
+
+
 function createSocket(server) {
     const io = new Server(server);
 
     const gameNamespace = io.of("/api/game");
     gameNamespace.on("connection", (socket) => {
         console.log("a user connected");
+        const game = new Game();
 
-        let game = new Game();
+        //TODO
+        socket.on("isMoveValid", (msg) => {
+            console.log("On vérifie si le mouvement est valide");
+        });
+
+        //TODO
+        socket.on("newMovePlayer1", (msg) => {
+            console.log("Appliquer le nouveau mouvement au joueur 1");
+        });
+
+        //TODO
+        socket.on("putAWall", (msg) => {
+            console.log("On applique un nouveau mur");
+        });
+
+        //TODO
+        socket.on("NewTurn", (msg) => {
+            console.log("On change de tour");
+            game.validateRound();
+        });
 
         socket.on("newMove", (msg) => {
             console.log("Player1 makes a new movement, computing AI's move and sending it back to the client");
