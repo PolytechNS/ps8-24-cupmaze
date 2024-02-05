1e88e7d603ece576f52d442df226782775165b10
simonbeurel <simon.beurel@etu.univ-cotedazur.fr>
2024-02-05 10:10:18 +0100

    Nouvel ajout de la logique du jeu dans le backend

--- back/logic/Case.js
+++ back/logic/Case.js
@@ -1,4 +1,5 @@
 class Case{
+
     constructor(pos_x, pos_y, isOccupied){
         this.pos_x = pos_x;
         this.pos_y = pos_y;
@@ -19,6 +20,14 @@ class Case{
 
     setIsOccupied(isOccupied){
         this.isOccupied = isOccupied;
+    }
+
+    equals(caseWanted){
+        return this.pos_x === caseWanted.getPos_x() && this.pos_y === caseWanted.getPos_y();
+    }
+
+    isAdjacent(caseWanted){
+        return (Math.abs(this.pos_x - caseWanted.getPos_x()) === 1 && this.pos_y === caseWanted.getPos_y()) || (Math.abs(this.pos_y - caseWanted.getPos_y()) === 1 && this.pos_x === caseWanted.getPos_x());
     }
 }
 
