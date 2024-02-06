347e283b50900fa51e18774fb5d0c6a8df221304
Arnaud <arnaud.dumanois@etu.univ-cotedazur.fr>
2024-02-06 19:15:05 +0100

    feat: creation de la fonction de creation d'un User ref #24

---
+++ back/database/mongo.js
@@ -0,0 +1,19 @@
+const { MongoClient } = require('mongodb');
+const MONGO_URI = 'mongodb://localhost:27017';
+
+const client = new MongoClient(MONGO_URI);
+
+async function getDb() {
+  if (!client.isConnected()) {
+    await client.connect();
+  }
+  return client.db('test');
+}
+
+async function createUser(user) {
+  const db = await getDb();
+  const users = db.collection('users');
+  return users.insertOne(user);
+}
+
+exports.createUser = createUser;
