56f62c89cb1a80d85e2f9cc464cf6a5fea0913a0
Arnaud <arnaud.dumanois@etu.univ-cotedazur.fr>
2024-02-07 20:14:47 +0100

    feat: migration backend du wallLayingUtils ref #??

--- back/logic/wallLayingUtils.js
+++ back/logic/wallLayingUtils.js
@@ -1,5 +1,8 @@
+import {Wall} from "./Wall";
+
 export {isWallPlacementValid}
 
 function isWallPlacementValid(firstWall, secondWall, space) {
-
+    const isLaid = firstWall.getIsLaid() || secondWall.getIsLaid() || space.getIsLaid();
+    return !isLaid;
 }
