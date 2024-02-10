da25fc7fc5a10a7bac957e5a002a317e64f72ec0
SayfHalmi <sayf-eddine.halmi@etu.unice.fr>
2024-02-10 18:21:09 +0100

    #35 create layWall socket method to emit from front

--- back/Sockets/socketGameBot.js
+++ back/Sockets/socketGameBot.js
@@ -1,6 +1,7 @@
 const AIEasy = require("../logic/ai.js");
 const { Server } = require("socket.io");
 const { Game } = require("../logic/Game.js");
+const {findWall, findSpace} = require("../logic/utils");
 
 /**
  * Cette fonction va servir pour pouvoir créer le socket qui correspond à quand on va vouloir initialiser une partie entre le bot et un joueur en local
@@ -14,11 +15,44 @@ function createSocket(server) {
     gameNamespace.on("connection", (socket) => {
         console.log("a user connected");
         const game = new Game();
-
         socket.on("newMove", (msg) => {
             console.log("On demande à l'IA de jouer maintenant");
-            let newPosition = AIEasy.computeMove(msg);
+            let newPosition = AIEasy.computeMove(msg,game);
+            console.log("new Position: "+newPosition);
             gameNamespace.emit("updatedBoard", newPosition);
+        });
+
+        socket.on("layWall", (msg) => {
+            console.log("Le joueur souhaite placer le mur suivant : "+msg);
+            // msg is string of form: wv~X-Y A-B-space wv~M-N
+            let isWallLayingPossible=game.actionsToDo>0 && game.currentPlayer===1 && game.nbWallsPlayer1>0;
+            if(isWallLayingPossible){
+                let walls=msg.split(" ");
+                let coords;
+                let line;
+                let col;
+                walls.forEach((wall)=>{
+                    //console.log(wall);
+                    coords=wall.split("~")[1];
+                    if(!coords) coords=wall;
+                    line=parseInt(coords.split("-")[0]);
+                    col=parseInt(coords.split("-")[1]);
+                    if(wall[1]==="h"){
+                        //Horizontal Wall
+                        findWall(line,col,"horizontal",game.elements).isLaid=true;
+                    }
+                    else if(wall[1]==="v"){
+                        //Vertical Wall
+                        findWall(line,col,"vertical",game.elements).isLaid=true;
+                    }
+                    else{
+                        //Space
+                        findSpace(line,col,game.elements).isLaid=true;
+                    }
+                })
+                game.nbWallsPlayer1--;
+            }
+            gameNamespace.emit("wallLaying", isWallLayingPossible, game.nbWallsPlayer1);
         });
 
         socket.on("disconnect", () => {
