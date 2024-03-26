793e4e298ecf5f61b2f987c7924d0d9a00d02654
SayfHalmi <sayf-eddine.halmi@etu.unice.fr>
2024-03-26 23:44:05 +0100

    fix first move undo crash

--- back/Sockets/socketGameBot.js
+++ back/Sockets/socketGameBot.js
@@ -103,16 +103,16 @@ function createSocket(io) {
         socket.on("undoMovePosition", () => {
             let oldPositionHTML=game.playerPosition["player1"][0]+"-"+game.playerPosition["player1"][1]+"~cell";
             let newPositionHtml="";
-            const lastCase = game.getCase(game.lastPlayerPosition["player1"][0], game.lastPlayerPosition["player1"][1]);
-            lastCase.setIsOccupied(false);
-            if(game.lastPlayerPosition["player1"]!==null){
-                newPositionHtml=game.lastPlayerPosition["player1"][0]+"-"+game.lastPlayerPosition["player1"][1]+"~cell";
-            }
-            game.playerPosition["player1"] = game.lastPlayerPosition["player1"]
+            game.playerPosition["player1"] = game.lastPlayerPosition["player1"];
             game.actionsToDo=1;
-            const currentCase = game.getCase(game.playerPosition["player1"][0], game.playerPosition["player1"][1]);
-            currentCase.setIsOccupied(true);
-            game.graph.updateNodeState(game.playerPosition["player1"][0], game.playerPosition["player1"][1], 0);
+            if(game.lastPlayerPosition["player1"]!== null){
+                const lastCase = game.getCase(game.lastPlayerPosition["player1"][0], game.lastPlayerPosition["player1"][1]);
+                lastCase.setIsOccupied(false);
+                newPositionHtml=game.lastPlayerPosition["player1"][0]+"-"+game.lastPlayerPosition["player1"][1]+"~cell";
+                const currentCase = game.getCase(game.playerPosition["player1"][0], game.playerPosition["player1"][1]);
+                currentCase.setIsOccupied(true);
+                game.graph.updateNodeState(game.playerPosition["player1"][0], game.playerPosition["player1"][1], 0);
+            }
             BotGameNamespace.emit("undoMove", oldPositionHTML, newPositionHtml, 1, game.numberTour);
         });
 
